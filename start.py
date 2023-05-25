3ccc3c72cf2f246e10386d16266190ea33acb39f
Fedex0065 <federico.brambilla@liceobanfi.eu>
2023-05-25 09:48:30 +0200

    eliminati file inutili

--- start.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-class Start:
-    def __init__(self, screen, pos, size) -> None:
-        self.screen= screen
-        self.rect = pygame.Rect(pos[0], pos[1], size[0], size[1])
-        
